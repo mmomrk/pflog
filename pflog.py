a985e8f75411694e9e998a5c35576ce7251db33c
mmomrk <mr.ak-47@rambler.ru>
2020-04-24 19:07:54 +0300

    Add rearrange. Add test_pflog.py with tests. Seem to work. Perhaps changing priorities would be required in the future

--- pflog.py
+++ pflog.py
@@ -1,76 +1,133 @@
 #!/usr/bin/env python3
 
+import collections
+
 
 class Pflog():
     def __init__(self, fname):
         self.fname = fname
         self.buf = {}
-        self.names = {}
+        self.names = collections.OrderedDict()
         self.firstLoop = True
         self.newlineName = ""
         self.capacity = 0
+        self.finalNameOrder = []
 
     @staticmethod
     def swap(ilist, pair):
-        print (ilist)
+        print(ilist)
         ilist[pair[0]], ilist[pair[1]] = ilist[pair[1]], ilist[pair[0]]
-        print (ilist)
+        print(ilist)
         return ilist
 
     @staticmethod
-    def toWrStr(inames,idict):
-        buf = [ idict[name] for name in inames]
+    def toWrStr(inames, idict):
+        # Expects input ordered names and dictionary with fresh values. Will write values from dictionary getting it by keys in the order from inames
+        buf = [idict[name] for name in inames]
         return "\t".join([str(x) for x in buf])+"\n"
 
     @staticmethod
-    def rearrange(iarr, ipos):
-        curs =list( range(len(iarr)))
-        from collections import defaultdict as dedi
-        accums = dedi(lambda : 0)
-        for x in ipos:
-            accums[x] += 1
-        print("Acums:",accums)
-        #for 
-        print ("TODO FINISHED HERE !!!")
-        return iarr
+    def rearrange(inamesDict):
+        iarr = inamesDict.keys()
+        ipos = inamesDict.values()
+        # current positions
+        n = len(iarr)
+        curs = list(range(n))
+        # counters of currently vacant position
+        negC = -1
+        posC = 1
+        ret = [""]*n
+        # requests = sorted(inamesDict.items(), key=lambda t : t[1])
+        # print ("Sorted requests are", requests)
+        # for cand in requests:
+        print("Ginput:", inamesDict)
+        fillers = []
+        for cand in inamesDict.items():
+            print(cand)
+            print(ret)
+            r = cand[1]
+            name = cand[0]
+            if r == 0:
+                fillers.append(name)
+                continue
+            # 0 is default and means no priority:
+            if r > 0:
+                r = r-1
+            if r < -n:
+                r = -n
+            # r is used as index in the array now:
+            if r > n - 1:
+                r = n - 1
+            if ret[r] == "":
+                ret[r] = name
+            else:
+                if r >= 0:
+                    for r1 in range(r+1, n):
+                        print("trying ", r1)
+                        if ret[r1] == "":
+                            ret[r1] = name
+                            break
+                    else:
+                        print("WARNING: could not resolve conflict with " +
+                              name+". Will put it to a free unclaimed column")
+                        fillers.append(name)
+                else:  # r is < 0 for sure
+                    print("Running now on ", list(range(n+r-1, -1, -1)))
+                    for r1 in range(n+r-1, -1, -1):
+                        print("trying ", r1)
+                        if ret[r1] == "":
+                            ret[r1] = name
+                            break
+                    else:
+                        print("WARNING: could not resolve conflict with " +
+                              name+". Will put it to a free unclaimed column")
+                        fillers.append(name)
+        fj = 0
+        for i, n in enumerate(ret):
+            if n == "":
+                ret[i] = fillers[fj]
+                fj += 1
+        print("Compiled ret:", ret)
+        return ret
 
-        
-        
-
-    def asd(self, val, nam, index = 0):
+    def asd(self, val, nam, ind=0):
         # 'asd' is a shorter version of 'add'
         assert nam != "", "Empty name is not allowed"
-        #print ("adding",val,nam)
+        # print ("adding",val,nam)
 
-        if nam in self.names.keys and self.firstLoop:
+        if nam in self.names.keys() and self.firstLoop:
             self.firstLoop = False
-            self.newlineName = self.names[-1]
-            self.names = rearrange(self.names.keys, self.names.values)
+
+            self.newlineName = self.prevName
+            print("rearrange")
+            self.finalNameOrder = Pflog.rearrange(self.names)
             self.ofle = open(self.fname, 'w')
-            self.ofle.write(Pflog.toWrStr(self.names.keys, {nam:nam for nam in self.names.keys}))
-            self.ofle.write(Pflog.toWrStr(self.names.keys,self.buf))
+            self.ofle.write(Pflog.toWrStr(self.finalNameOrder, {
+                            nam: nam for nam in self.names.keys()}))
+            self.ofle.write(Pflog.toWrStr(self.finalNameOrder, self.buf))
             self.capacity = len(self.buf)
             assert self.capacity == len(
                 self.names), "Bad init. Different name and val sizes"
-            #print("DEBUG: set lap")
-            #print ("DEBUG: newline name is ", self.newlineName)
+            # print("DEBUG: set lap")
+            # print ("DEBUG: newline name is ", self.newlineName)
             self.buf = {}
             self.asd(val, nam)
             return
 
         if self.firstLoop:
-            self.names[nam] = index
-        self.buf[nam] =  val
+            self.names[nam] = ind
+            self.prevName = nam
+        self.buf[nam] = val
 
         if nam == self.newlineName:
             assert len(
                 self.buf) == self.capacity, "Bad write with incomplete buf"+str(self.buf)+self.capacity
-            ostr = Pflog.toWrStr(self.names.keys,self.buf)
-            #print ("DEBUG: writing to file", ostr)
+            ostr = Pflog.toWrStr(self.finalNameOrder, self.buf)
+            # print ("DEBUG: writing to file", ostr)
             self.ofle.write(ostr)
             self.buf = {}
-        #print (self.buf)
-        #print (self.names)
+        # print (self.buf)
+        # print (self.names)
 
     def close(self):
         self.ofle.close()
---
+++ test_pflog.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+
+import unittest
+import pflog as pf
+import sys
+import os
+
+
+class testPflog(unittest.TestCase):
+    def testInit(self):
+        fnam = "hello.delit"
+        x = pf.Pflog(fnam)
+        assert x is not None, "Bad init"
+        assert x.buf == {} and x.names == {
+        } and x.firstLoop == True and x.newlineName == "", "A very stupid test failed"
+        x.asd(1, '1')
+        x.asd(2, '1')
+        assert x.firstLoop == False, "No first loop detection"
+        x.close()
+        assert os.path.isfile(fnam), "out file not created"
+        fle = open(fnam, 'r')
+        lines = fle.readlines()
+        fle.close()
+        os.remove(fnam)
+        assert "1" in lines[0] and "1" in lines[1] and "2" in lines[2], "File written incorrectly"
+
+    def testNWrites(self):
+        num = 10
+        names = [str(i)+".delit" for i in range(num)]
+        pfls = [pf.Pflog(name) for name in names]
+        for _ in range(100):
+            for i, pfl in enumerate(pfls):
+                for t in range(i+1):
+                    pfl.asd(t, str(t)+"_"+str(t))
+        for pfl in pfls:
+            pfl.close()
+
+        for name in names:
+            assert os.path.isfile(name), name+" was not created in the test"
+            fle = open(name, 'r')
+            lines = fle.readlines()
+            fle.close()
+            os.remove(name)
+            assert len(lines[0].split()) == int(
+                name.split('.')[0])+1, "Bad number of columns"
+
+    def testRearrange(self):
+        n = int(10)
+        names = [str(i) for i in range(-int(n/2), int(n/2))]
+        fnam = 'hard.delit'
+        ol = pf.Pflog(fnam)
+        for _ in range(5):
+            for val, name in zip(range(n), names):
+                ol.asd(val, name, ind=int(int(name)/2))
+        ol.close()
+
+        assert os.path.isfile(fnam), name+" was not created in the test"
+        fle = open(fnam, 'r')
+        lines = fle.readlines()
+        fle.close()
+        os.remove(fnam)
+        assert lines[0] == '\t'.join([str(i) for i in [2, 3, 4, -1, 0, 1, -2, -4, -5, -3]]) + \
+            '\n', "Bad rearrange result. May be a consequence of changing the queueing and priorities; " + \
+            lines[0]
+
+    def testIndSame(self):
+        fnam = 'ind.delit'
+        ol = pf.Pflog(fnam)
+        for _ in range(10):
+            ol.asd(1, '1', ind=-1)
+            ol.asd(2, '2', ind=-1)
+            ol.asd(3, '3', ind=-1)
+        ol.close()
+        assert os.path.isfile(fnam), name+" was not created in the test"
+        fle = open(fnam, 'r')
+        lines = fle.readlines()
+        fle.close()
+        os.remove(fnam)
+        assert lines[0] == '\t'.join(
+            [str(i) for i in [3, 2, 1]]) + '\n', "Failed test of repeated ind=-1"
+
+    def testIndOverflow(self):
+        fnam = 'indof.delit'
+        ol = pf.Pflog(fnam)
+        for _ in range(10):
+            ol.asd(1, '1', ind=11)
+            ol.asd(2, '2', ind=-11)
+            ol.asd(3, '3')
+        ol.close()
+        assert os.path.isfile(fnam), name+" was not created in the test"
+        fle = open(fnam, 'r')
+        lines = fle.readlines()
+        fle.close()
+        os.remove(fnam)
+        assert lines[0] == '\t'.join(
+            [str(i) for i in [2, 3, 1]]) + '\n', "Failed test of repeated ind=-1"
+
+
+if __name__ == "__main__":
+    unittest.main()
